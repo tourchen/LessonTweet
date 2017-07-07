11dc429a3f04a461f2c98e90ddbf26a310d1f6a5
TourChen <tourchen@163.com>
2017-07-07 18:42:04 +0800

    Initial commit

---
+++ app/src/androidTest/java/cn/zju/id8311250/lessontweet/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cn.zju.id8311250.lessontweet;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
